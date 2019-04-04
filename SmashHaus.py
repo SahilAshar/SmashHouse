e9606be148b4ef2a33a9235033ddbdb839e575fe
NOBrien27 <nicholasobrien27@yahoo.com>
2019-04-04 17:57:15 -0500

    OOP version

--- SmashHaus.py
+++ SmashHaus.py
@@ -1,67 +1,23 @@
-from Player import Player
 from DataHandler import DataHandler
+from tkinter import *
 
 def main():
 
-    inf = open("information.csv", "r")
     dataHandler = DataHandler()
 
-
-    num_players = inf.readline().strip()
-    num_players = int(num_players[0])
-
-    # Create a list to store every player object
-
-    player_list = []
-
-    for i in range(num_players):
-       
-        # Create player objects and store them in player_list
-
-        x = inf.readline().strip()
-        x = x.split(",")
-        user = Player(x[0],x[1],int(x[2]),int(x[3]),int(x[4]),int(x[5]))
-        print(user.username)
-        player_list.append(user)
-        user_data[x[0]] = user
-
-    # Generate a list of lowercase usernames for verification later
-
-    lower_usernames = []
-
-    for i in range(len(player_list)):
-        lower_usernames.append(player_list[i].username.lower())
-
-    if num_players == 0:
+    if dataHandler.num_players == 0:
         print("\n# ============================================================ #")
         print("There are no registered users. Start by adding two new players.")
         print("# ============================================================ #")
-        
-        player_list.append(add_player(lower_usernames, num_players))
-        user_data[player_list[0].username] = player_list[0]
+        dataHandler.add_player(1)
+        dataHandler.add_player(2)
 
-        num_players += 1
-
-        # Reset lower_usernames such that the second player can't duplicate the first.
-
-        lower_usernames = []
-
-        for i in range(len(player_list)):
-            lower_usernames.append(player_list[i].username.lower())
-        
-        player_list.append(add_player(lower_usernames, num_players))
-        user_data[player_list[1].username] = player_list[1]
-
-    elif num_players == 1:
+    elif dataHandler.num_players == 1:
         print("\n# ============================================================ #")
         print("There is only one registered user. Add one more player.")
         print("# ============================================================ #")
-        player_list.append(add_player(lower_usernames, num_players))
-        user_data[player_list[1].username] = player_list[1]
-        num_players += 1
+        dataHandler.add_player(1)
 
-
-    print(user_data)
     a = "Smash Hoüs 405"
     b = "What would you like to do?"
     c = "Enter the number of the task you would like to run."
@@ -83,17 +39,10 @@ def main():
     print("|| 7. Exit." + format(" ||", ">55"))
     print("# ============================================================== #")
 
-
     user_input = 0
     count = 0
     while user_input != 'n':
-        lower_usernames = []
-        usernames = []
-
-        for i in range(len(player_list)):
-            lower_usernames.append(player_list[i].username.lower())
-            usernames.append(player_list[i].username)
-        
+       
         if count > 0:
             user_input = input("\nWould you like to continue? Yes(y) | No(n).\n :: ")
             
@@ -126,26 +75,27 @@ def main():
             user_input = input("Yikes, try again \n:: ")
     
         if user_input == '1':
-            current_standings(player_list)
+            dataHandler.current_standings()
     
         elif user_input == '2':
-            match_results()
+            dataHandler.report_match()
 
         elif user_input == '3':
-            get_match_history()
+            dataHandler.get_match_history()
 
         elif user_input == '4':
-            get_player_stats()
+            dataHandler.get_player_stats()
 
         elif user_input == '5':
-            info = add_player()
+            dataHandler.add_player()
     
         elif user_input == '6':
-            info = remove_player()
+            dataHandler.remove_player()
         
         elif user_input == '7':
             break
         
         count += 1
+
 
 main()
